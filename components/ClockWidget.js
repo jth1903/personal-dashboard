08457f1ae37c101ef9a25114689e88d974e6bdc0
Jeffrey H <jth1903@hotmail.com>
2026-01-19 19:59:15 -0600

    fixed hydration error

--- components/ClockWidget.js
+++ components/ClockWidget.js
@@ -5,8 +5,11 @@ import { useEffect, useState } from 'react';
 export default function ClockWidget() {
     const [time, setTime] = useState(new Date());
     const [quote, setQuote] = useState(null);
+    const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
+        setMounted(true);
+
         const API_KEY = process.env.NEXT_PUBLIC_API_NINJAS_KEY;
 
         fetch('https://api.api-ninjas.com/v2/quoteoftheday', {
@@ -37,6 +40,13 @@ export default function ClockWidget() {
         <div className="p-4 bg-black rounded-lg shadow">
             <h2 className="text-lg font-bold mb-2">Current Time</h2>
             <div className="text-4xl font-bold">
+                {mounted
+                    ? time.toLocaleTimeString('en-US', {
+                          hour: '2-digit',
+                          minute: '2-digit',
+                          second: '2-digit',
+                      })
+                    : ' --:--:-- '}
                 {time.toLocaleTimeString('en-US', {
                     hour: '2-digit',
                     minute: '2-digit',
